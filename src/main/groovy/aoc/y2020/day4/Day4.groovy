2cf5b716d20f50c5a46e70ec176b64ae4fb6bc84
James Cook <james.cookie@gmail.com>
2020-12-04 08:12:01 +0000

    2020/4 tidy up

--- src/main/groovy/aoc/y2020/day4/Day4.groovy
+++ src/main/groovy/aoc/y2020/day4/Day4.groovy
@@ -38,42 +38,34 @@ class Day4 {
 
         boolean validPart2() {
             try {
-                return byr && Integer.parseInt(byr) >= 1920 && Integer.parseInt(byr) <= 2002 &&
-                        iyr && Integer.parseInt(iyr) >= 2010 && Integer.parseInt(iyr) <= 2020 &&
-                        eyr && Integer.parseInt(eyr) >= 2020 && Integer.parseInt(eyr) <= 2030 &&
-                        hgt && validHeight(hgt) &&
-                        hcl && hcl ==~ /#[0-9a-f]{6}/ &&
-                        ecl && ecl in ['amb', 'blu','brn','gry','grn','hzl','oth'] &&
-                        pid && pid ==~ /[0-9]{9}/
+                return validPart1() &&
+                        between(byr, 1920, 2002) &&
+                        between(iyr, 2010, 2020) &&
+                        between(eyr, 2020, 2030) &&
+                        validHeight(hgt) &&
+                        hcl ==~ /#[0-9a-f]{6}/ &&
+                        ecl in ['amb', 'blu','brn','gry','grn','hzl','oth'] &&
+                        pid ==~ /[0-9]{9}/
             } catch (e) {
                 return false
             }
         }
 
-        boolean validHeight(hgt) {
+        static boolean validHeight(String hgt) {
             def matcher = hgt =~ /(\d+)([cm|in]+)/
             if (matcher.size() == 1 && matcher[0].size() == 3) {
                 if (matcher[0][2] == 'in') {
-                    return Integer.parseInt(matcher[0][1]) >= 59 && Integer.parseInt(matcher[0][1]) <= 76
+                    return between(matcher[0][1], 59, 76)
                 }
                 if (matcher[0][2] == 'cm') {
-                    return Integer.parseInt(matcher[0][1]) >= 150 && Integer.parseInt(matcher[0][1]) <= 193
+                    return between(matcher[0][1], 150, 193)
                 }
             }
             return false
         }
 
-        /*
-        byr (Birth Year) - four digits; at least 1920 and at most 2002.
-iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-hgt (Height) - a number followed by either cm or in:
-If cm, the number must be at least 150 and at most 193.
-If in, the number must be at least 59 and at most 76.
-hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-pid (Passport ID) - a nine-digit number, including leading zeroes.
-cid (Country ID) - ignored, missing or not.
-         */
+        static boolean between(String s, int low, int high) {
+            s && Integer.parseInt(s) >= low && Integer.parseInt(s) <= high
+        }
     }
 }
