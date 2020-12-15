d873fbe1c4cec4a29558fe596ed3e9610c37de0c
James Cook <james.cookie@gmail.com>
2020-12-15 13:49:14 +0000

    2020/15

--- src/main/groovy/aoc/y2020/day15/Day15.groovy
+++ src/main/groovy/aoc/y2020/day15/Day15.groovy
@@ -21,17 +21,19 @@ class Day15 {
 
     static part2(String inputString, int target) {
         Integer[] input = inputString.trim().tokenize(',').collect { Integer.parseInt(it) }
-        Map<Integer, Pair> spoken = [:]
+        Map<Integer, Pair> spoken = new LinkedHashMap<>()
         int inputSize = input.size()
         for (i in 0..<inputSize) {
-            spoken.put(input[i], new Pair().push(i + 1))
+            spoken.put(input[i], new Pair(i + 1))
         }
         int next = input[-1]
-        Pair p
-        for (i in inputSize..<target) {
-            p = spoken.get(next)
-            next = p.i2 ? i - p.i2 : 0
-            spoken.put(next, spoken.getOrDefault(next, new Pair()).push(i + 1))
+        for (int i = inputSize; i <target; i++) {
+            next = i - (spoken.get(next).i2 ?: i)
+            if (spoken.containsKey(next)) {
+                spoken.get(next).push(i + 1)
+            } else {
+                spoken.put(next, new Pair(i + 1))
+            }
         }
         return next
     }
@@ -39,6 +41,10 @@ class Day15 {
     static class Pair {
         int i1
         int i2
+
+        Pair(int i) {
+            i1 = i
+        }
 
         Pair push(int i) {
             i2 = i1
