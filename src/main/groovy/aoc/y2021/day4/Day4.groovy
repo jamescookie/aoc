e91b2a9abda1575b1f3cc77bd862cf6c61b3e16a
James Cook <james.cookie@gmail.com>
2021-12-04 16:50:19 +0000

    2021/4 part 1/2

--- src/main/groovy/aoc/y2021/day4/Day4.groovy
+++ src/main/groovy/aoc/y2021/day4/Day4.groovy
@@ -2,12 +2,43 @@ package aoc.y2021.day4
 
 class Day4 {
     static part1(String inputString) {
-        Integer[] input = inputString.tokenize().collect {Integer.parseInt(it)}
-        return null
+        return bingo(inputString) { a, b -> a.lastCalledIndex <=> b.lastCalledIndex }
     }
 
     static part2(String inputString) {
-        Integer[] input = inputString.tokenize().collect {Integer.parseInt(it)}
-        return null
+        return bingo(inputString) { a, b -> b.lastCalledIndex <=> a.lastCalledIndex }
     }
+
+    static int bingo(String inputString, Closure<Integer> sorting) {
+        String[] input = inputString.split("\n\n")
+        def calling = input[0].tokenize(",").collect { Integer.parseInt(it) }
+        return input[1..-1].collect { new Board(it, calling) }.sort(sorting)[0].answer
+    }
+
+    private static class Board {
+        int lastCalledIndex
+        int answer
+        Collection<Integer> answers = []
+
+        Board(String input, Collection<Integer> numbers) {
+            int[][] board = input.tokenize("\n").collect { row -> row.tokenize().collect { Integer.parseInt(it) } }
+            for (i in 0..<board.length) {
+                addAnswer(numbers, board[i])
+            }
+            for (i in 0..<board[0].length) {
+                addAnswer(numbers, board*.getAt(i))
+            }
+            if (answers) {
+                lastCalledIndex = answers.min()
+                answer = (board.flatten() - numbers[0..lastCalledIndex]).sum() * numbers[lastCalledIndex]
+            }
+        }
+
+        protected void addAnswer(Collection<Integer> numbers,  toCheck) {
+            if (numbers.containsAll(toCheck)) {
+                answers << toCheck.collect { x -> numbers.findIndexOf { it == x } }.max()
+            }
+        }
+    }
+
 }
--- src/test/groovy/aoc/y2021/day4/Day4Spec.groovy
+++ src/test/groovy/aoc/y2021/day4/Day4Spec.groovy
@@ -6,7 +6,25 @@ import spock.lang.Unroll
 
 @Unroll
 class Day4Spec extends Specification {
-    static def t1 = """"""
+    static def t1 = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"""
 
     def "part1 warmup tests"() {
         expect:
@@ -14,12 +32,12 @@ class Day4Spec extends Specification {
 
         where:
         input  | output
-        t1     | 0
+        t1     | 4512
     }
 
     def "part1"() {
         expect:
-        Day4.part1(InputReader.read("y2021/day4")) == 0
+        Day4.part1(InputReader.read("y2021/day4")) == 49686
     }
 
     def "part2 warmup tests"() {
@@ -28,11 +46,11 @@ class Day4Spec extends Specification {
 
         where:
         input  | output
-        t1     | 0
+        t1     | 1924
     }
 
     def "part2"() {
         expect:
-        Day4.part2(InputReader.read("y2021/day4")) == 0
+        Day4.part2(InputReader.read("y2021/day4")) == 26878
     }
 }
