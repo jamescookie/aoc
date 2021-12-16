088658fca0535d1f10bb75baa2e356186820fafd
James Cook <james.cookie@gmail.com>
2021-12-16 15:41:47 +0000

    2021/16 part 2

--- src/main/groovy/aoc/y2021/day16/Day16.groovy
+++ src/main/groovy/aoc/y2021/day16/Day16.groovy
@@ -1,6 +1,8 @@
 package aoc.y2021.day16
 
 class Day16 {
+    static final Closure<Boolean> DEFAULT_STOP = { StringBuffer remaining, count -> remaining.length() == 0 || !remaining.toString().contains('1') }
+
     static part1(String inputString) {
         StringBuffer input = new StringBuffer(inputString.trim().collect { Integer.toBinaryString(Integer.parseInt(it, 16)).padLeft(4, '0') }.join())
         int versions = 0
@@ -13,7 +15,7 @@ class Day16 {
         return readPacket(input, {}).sum()
     }
 
-    static List<Long> readPacket(StringBuffer input, Closure checkVersion, Closure stopCondition = { StringBuffer remaining, count -> remaining.length() == 0 || !remaining.toString().contains('1') }) {
+    static List<Long> readPacket(StringBuffer input, Closure checkVersion, Closure stopCondition = DEFAULT_STOP) {
         List<Long> results = []
         int count = 0
         while (true) {
@@ -27,7 +29,7 @@ class Day16 {
                     def numberOfSubPackets = Integer.parseInt(read(input, 11), 2)
                     subResults = readPacket(input, checkVersion, { a, b -> b == numberOfSubPackets })
                 } else {
-                    subResults = readPacket(new StringBuffer(read(input, Integer.parseInt(read(input, 15), 2))), checkVersion, { a, b -> a.length() == 0 || !a.toString().contains('1') })
+                    subResults = readPacket(new StringBuffer(read(input, Integer.parseInt(read(input, 15), 2))), checkVersion, DEFAULT_STOP)
                 }
             }
             switch (type) {
--- src/test/groovy/aoc/y2021/day16/Day16Spec.groovy
+++ src/test/groovy/aoc/y2021/day16/Day16Spec.groovy
@@ -6,21 +6,19 @@ import spock.lang.Unroll
 
 @Unroll
 class Day16Spec extends Specification {
-    static def t1 = """"""
-
     def "part1 warmup tests"() {
         expect:
         Day16.part1(input) == output
 
         where:
         input                            | output
-//        "D2FE28"                         | 6
+        "D2FE28"                         | 6
         "38006F45291200"                 | 1 + 6 + 2
-//        "EE00D40C823060"                 | 7 + 2 + 4 + 1
-//        "8A004A801A8002F478"             | 16
-//        "620080001611562C8802118E34"     | 12
-//        "C0015000016115A2E0802F182340"   | 23
-//        "A0016C880162017C3686B18A3D4780" | 31
+        "EE00D40C823060"                 | 7 + 2 + 4 + 1
+        "8A004A801A8002F478"             | 16
+        "620080001611562C8802118E34"     | 12
+        "C0015000016115A2E0802F182340"   | 23
+        "A0016C880162017C3686B18A3D4780" | 31
     }
 
     def "part1"() {
@@ -33,16 +31,23 @@ class Day16Spec extends Specification {
         Day16.part2(input) == output
 
         where:
-        input                        | output
-        "C200B40A82"                 | 3
-        "04005AC33890"               | 54
-        "880086C3E88112"             | 7
-        "CE00C43D881120"             | 9
-        "D8005AC2A8F0"               | 1
-        "38006F45291200"             | 1
-        "F600BC2D8F"                 | 0
-        "9C005AC2F8F0"               | 0
-        "9C0141080250320F1802104A08" | 1
+        input                            | output
+        "C200B40A82"                     | 3
+        "04005AC33890"                   | 54
+        "880086C3E88112"                 | 7
+        "CE00C43D881120"                 | 9
+        "D8005AC2A8F0"                   | 1
+        "F600BC2D8F"                     | 0
+        "9C005AC2F8F0"                   | 0
+        "9C0141080250320F1802104A08"     | 1
+        //from part 1
+        "D2FE28"                         | 2021
+        "38006F45291200"                 | 1
+        "EE00D40C823060"                 | 3
+        "8A004A801A8002F478"             | 15
+        "620080001611562C8802118E34"     | 46
+        "C0015000016115A2E0802F182340"   | 46
+        "A0016C880162017C3686B18A3D4780" | 54
     }
 
     def "part2"() {
