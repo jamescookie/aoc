f6c02d117a8aec9bcd7fcdd0c8ea66db30b1483a
James Cook <james.cookie@gmail.com>
2022-12-01 11:10:34 +0000

    2022/1 Refactor

--- src/main/groovy/aoc/y2022/day1/Day1.groovy
+++ src/main/groovy/aoc/y2022/day1/Day1.groovy
@@ -2,12 +2,14 @@ package aoc.y2022.day1
 
 class Day1 {
     static part1(String inputString) {
-        def input = inputString.split('\n\n')*.split('\n')*.collect {it as int}*.sum()
-        return input.max()
+        return splitToGroups(inputString).max()
     }
 
     static part2(String inputString) {
-        def input = inputString.split('\n\n')*.split('\n')*.collect {it as int}*.sum()
-        return input.sort().reverse()[0..2].sum()
+        return splitToGroups(inputString).sort().reverse()[0..2].sum()
+    }
+
+    protected static List<Long> splitToGroups(String inputString) {
+        inputString.split('\n\n').collect { (Long) it.split('\n').collect { it as Long }.sum() }
     }
 }
