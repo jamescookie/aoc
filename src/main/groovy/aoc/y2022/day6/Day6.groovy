b3915e5db5769c700730f7a105b42687d7c741de
James Cook <james.cookie@gmail.com>
2022-12-06 08:26:48 +0000

    2022/6 tidy

--- src/main/groovy/aoc/y2022/day6/Day6.groovy
+++ src/main/groovy/aoc/y2022/day6/Day6.groovy
@@ -2,39 +2,29 @@ package aoc.y2022.day6
 
 class Day6 {
     static part1(String inputString) {
-        int count = 0
-        def packet = []
-        for (i in 0..<inputString.length()) {
-            count++
-            if (packet.size() == 4) {
-                packet.remove(3)
-            }
-            packet.push(inputString[i])
-            if (packet.size() == 4) {
-                if (allDifferent(packet)) {
-                    break
-                }
-            }
-        }
-        return count
+        return findDistinct(inputString, 4)
     }
 
     static part2(String inputString) {
-        int count = 0
+        return findDistinct(inputString, 14)
+    }
+
+    static int findDistinct(String inputString, int packetSize) {
         def packet = []
+        int count = 0
         for (i in 0..<inputString.length()) {
             count++
-            if (packet.size() == 14) {
-                packet.remove(13)
+            if (packet.size() == packetSize) {
+                packet.remove(packetSize - 1)
             }
             packet.push(inputString[i])
-            if (packet.size() == 14) {
+            if (packet.size() == packetSize) {
                 if (allDifferent(packet)) {
                     break
                 }
             }
         }
-        return count
+        count
     }
 
     static boolean allDifferent(List list) {
