e2b621573ab022d1cfd5a0de5800379997fdb8a5
James Cook <james.cookie@gmail.com>
2022-12-11 00:41:37 +0000

    2022/10

---
+++ src/main/groovy/aoc/y2022/day10/Day10.groovy
@@ -0,0 +1,67 @@
+package aoc.y2022.day10
+
+import java.util.concurrent.atomic.AtomicInteger
+
+class Day10 {
+    static part1(String inputString) {
+        def input = inputString.split('\n')
+        long x = 1
+        long cycle = 1
+        AtomicInteger interest = new AtomicInteger(20)
+        long value = 0
+        for (i in 0..<input.size()) {
+            value = checkCycle(cycle, x, interest, value)
+            cycle++
+            if (input[i].startsWith('addx')) {
+                value = checkCycle(cycle, x, interest, value)
+                int amount = Integer.parseInt(input[i] - 'addx ')
+                x += amount
+                cycle++
+            }
+        }
+        return value
+    }
+
+    static part2(String inputString) {
+        def input = inputString.split('\n')
+        long x = 1
+        long cycle = 1
+        StringBuffer sb = new StringBuffer()
+        AtomicInteger interest = new AtomicInteger(40)
+        for (i in 0..<input.size()) {
+            printCycle(cycle, x, interest, sb)
+            cycle++
+            if (input[i].startsWith('addx')) {
+                printCycle(cycle, x, interest, sb)
+                int amount = Integer.parseInt(input[i] - 'addx ')
+                x += amount
+                cycle++
+            }
+        }
+        print(sb.toString())
+        return sb.toString()
+    }
+
+    static long checkCycle(long cycle, long x, AtomicInteger interest, long value) {
+        if (cycle == interest.get()) {
+            interest.set(interest.get() + 40)
+            return value + (x * cycle)
+        } else {
+            return value
+        }
+    }
+
+    static void printCycle(long cycle, long x, AtomicInteger rowLength, StringBuffer sb) {
+        while(cycle > 40) {
+            cycle -= 40
+        }
+        if (cycle == x || cycle == x+1 || cycle == x+2) {
+            sb.append('#')
+        } else {
+            sb.append('.')
+        }
+        if (cycle == 40) {
+            sb.append('\n')
+        }
+    }
+}
