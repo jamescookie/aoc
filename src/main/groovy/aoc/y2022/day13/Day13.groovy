8712f8e868313a880bdb701b871d714989cdf194
James Cook <james.cookie@gmail.com>
2022-12-13 14:47:51 +0000

    2022/13 Tidy

--- src/main/groovy/aoc/y2022/day13/Day13.groovy
+++ src/main/groovy/aoc/y2022/day13/Day13.groovy
@@ -2,12 +2,11 @@ package aoc.y2022.day13
 
 class Day13 {
     static part1(String inputString) {
-        def input = inputString.split('\n\n').collect { pairs -> pairs.split('\n').collect { Eval.me(it) } }
+        List<List<List<Object>>> input = inputString.split(System.lineSeparator() + System.lineSeparator()).collect { pairs -> pairs.split(System.lineSeparator()).collect { Eval.me(it) as List<Object> } }
         int result = 0
         for (i in 0..<input.size()) {
             if (correctOrder(input[i][0], input[i][1]) != Result.INCORRECT) {
-                def index = (i + 1)
-                result += index
+                result += (i + 1)
             }
         }
         return result
@@ -16,25 +15,21 @@ class Day13 {
     static part2(String inputString) {
         String divider1 = "[[2]]"
         String divider2 = "[[6]]"
-        List<Object> input = (inputString+"\n"+divider1+"\n"+divider2).replaceAll('\n\n', '\n').split('\n').collect { pairs -> pairs.split('\n').collect { Eval.me(it) } }
+        List<List<Object>> input = (inputString + System.lineSeparator() + divider1 + System.lineSeparator() + divider2).replaceAll(System.lineSeparator() + System.lineSeparator(), System.lineSeparator()).split(System.lineSeparator()).collect { pairs -> pairs.split(System.lineSeparator()).collect { Eval.me(it) } }
 
-        def sort = input.sort { a, b ->
-            switch (correctOrder(a, b)) {
-                case Result.CORRECT:
-                    return -1
-                case Result.EQUAL:
-                    return 0
-                case Result.INCORRECT:
-                    return 1
-            }
-        }.collect { (it as String)[1..-2] }
+        def sort = input.sort { a, b -> correctOrder(b, a).value }.collect { (it as String)[1..-2] }
         return (sort.indexOf(divider1) + 1) * (sort.indexOf(divider2) + 1)
     }
 
     static enum Result {
-        CORRECT,
-        INCORRECT,
-        EQUAL
+        CORRECT(1),
+        INCORRECT(-1),
+        EQUAL(0);
+        final int value;
+
+        Result(value) {
+            this.value = value
+        }
     }
 
     static Result correctOrder(List<Object> left, List<Object> right) {
@@ -49,12 +44,12 @@ class Day13 {
                     return Result.INCORRECT
                 }
             } else if (left[i] instanceof List && right[i] instanceof List) {
-                def answer = correctOrder(left[i], right[i])
+                def answer = correctOrder((List<Object>) left[i], (List<Object>) right[i])
                 if (answer != Result.EQUAL) {
                     return answer
                 }
             } else {
-                def answer = correctOrder(left[i] instanceof List ? left[i] : [left[i]], right[i] instanceof List ? right[i] : [right[i]])
+                def answer = correctOrder(left[i] instanceof List ? (List<Object>) left[i] : [left[i]], right[i] instanceof List ? (List<Object>) right[i] : [right[i]])
                 if (answer != Result.EQUAL) {
                     return answer
                 }
