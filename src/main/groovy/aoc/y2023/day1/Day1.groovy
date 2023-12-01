18598a08ffa2f2c99ecea877e08f48aff4f39f0e
James Cook <james.cook@mettle.co.uk>
2023-12-01 08:55:16 +0000

    2023/1 Part 1/2 - tidy

--- src/main/groovy/aoc/y2023/day1/Day1.groovy
+++ src/main/groovy/aoc/y2023/day1/Day1.groovy
@@ -2,34 +2,26 @@ package aoc.y2023.day1
 
 class Day1 {
     static def map = [
-            'one': '1',
-            'two': '2',
+            'one'  : '1',
+            'two'  : '2',
             'three': '3',
-            'four': '4',
-            'five': '5',
-            'six': '6',
+            'four' : '4',
+            'five' : '5',
+            'six'  : '6',
             'seven': '7',
             'eight': '8',
-            'nine': '9',
+            'nine' : '9',
     ]
 
     static part1(String inputString) {
-        String[] input = inputString.tokenize('\n')
-        def result = input.collect {line->
+        inputString.tokenize('\n').collect { line ->
             line = line.replaceAll(/[^0-9]+/, '')
             return (line[0] + line[-1]) as int
         }.sum()
-        return result
     }
 
     static part2(String inputString) {
-        String[] input = inputString.tokenize('\n')
-        def result = input.collect {line ->
-            String first = findFirst(line)
-            String last = findLast(line)
-            return (first + last) as int
-        }.sum()
-        return result
+        inputString.tokenize('\n').collect { (findFirst(it) + findLast(it)) as int }.sum()
     }
 
     protected static String findFirst(String line) {
@@ -46,7 +38,7 @@ class Day1 {
 
     protected static String findLast(String line) {
         for (i in 0..<line.size()) {
-            String tmp = line[0..(-1-i)]
+            String tmp = line[0..(-1 - i)]
             for (def es in map) {
                 if (tmp.endsWith(es.key) || tmp.endsWith(es.value)) {
                     return es.value
