ad32f35165e6ebce05eca3920e86ab1d6fcbd78f
James Cook <james.cook@mettle.co.uk>
2023-12-04 11:03:17 +0000

    2023/4 Part 1/2 - tidy

--- src/main/groovy/aoc/y2023/day4/Day4.groovy
+++ src/main/groovy/aoc/y2023/day4/Day4.groovy
@@ -2,35 +2,22 @@ package aoc.y2023.day4
 
 class Day4 {
     static part1(String inputString) {
-        def input = inputString.tokenize('\n')*.tokenize(':')*.last()*.tokenize('|')
-        long result = 0
-        for (i in 0..<input.size()) {
-            def winning = input[i][0].tokenize().collect {it as int}
-            def hand = input[i][1].tokenize().collect {it as int}
-            def intersect = winning.intersect(hand)
-            if (intersect) {
-                result += (1 << (intersect.size() - 1))
-            }
-        }
-        return result
+        return findIntersects(inputString).collect { it -> it ? (1 << (it - 1)) : 0 }.sum()
     }
 
     static part2(String inputString) {
-        def input = inputString.tokenize('\n')*.tokenize(':')*.last()*.tokenize('|')
-        long result = 0
-        List<List<Integer>> results = []
-        for (i in 0..<input.size()) {
-            def winning = input[i][0].tokenize().collect {it as int}
-            def hand = input[i][1].tokenize().collect {it as int}
-            def intersect = winning.intersect(hand)
-            results << [intersect.size(), 1]
-        }
+        List<List<Integer>> results = findIntersects(inputString).collect { [it, 1] }
         for (i in 0..<results.size()) {
-            int x = results[i][0]
-            for (j in 1..<(x+1)) {
-                results[i+j][1] = results[i+j][1] + results[i][1]
+            for (j in 1..<(results[i][0] + 1)) {
+                results[i + j][1] = results[i + j][1] + results[i][1]
             }
         }
-        return results.collect{it[1]}.sum()
+        return results.collect { it[1] }.sum()
+    }
+
+    protected static List<Integer> findIntersects(String inputString) {
+        inputString.tokenize('\n')*.tokenize(':')*.last()*.tokenize('|').collect {
+            it[0].tokenize().intersect(it[1].tokenize()).size()
+        }
     }
 }
