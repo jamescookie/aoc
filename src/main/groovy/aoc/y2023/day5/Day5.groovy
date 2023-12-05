564deb63f9504d9a579803725536b9ef83632558
James Cook <james.cook@mettle.co.uk>
2023-12-05 20:28:41 +0000

    2023/5 Part 2 rethink - split into ranges - working!

--- src/main/groovy/aoc/y2023/day5/Day5.groovy
+++ src/main/groovy/aoc/y2023/day5/Day5.groovy
@@ -27,20 +27,68 @@ class Day5 {
         List<List<Entry>> almanac = createAlmanac(input)
 
         long result = Long.MAX_VALUE;
+
         for (int i = 0; i < seedRanges.size(); i++) {
-            for (long j = (seedRanges[i]); j < (seedRanges[i] + seedRanges[i+1]); j++) {
-                long seed = j
-                for (List<Entry> thing : almanac) {
-                    for (Entry entry : thing) {
-                        if (seed >= entry.start && seed <= entry.end) {
-                            seed += entry.diff
-                            break
-                        }
-                    }
+            def ranges = [new BigRange(seedRanges[i], seedRanges[i] + seedRanges[i + 1] - 1)]
+            for (List<Entry> thing : almanac) {
+                def newRanges = []
+                for (BigRange range : ranges) {
+                    newRanges.addAll(findRanges(thing, [range]))
                 }
-                if (seed < result) result = seed
+                ranges = newRanges
             }
             i++
+            def min = ranges.collect{it.from}.min()
+            if (min < result) {
+                result = min
+            }
+        }
+
+        return result
+    }
+
+    protected static List<BigRange> findRanges(List<Entry> entries, List<BigRange> currentRanges) {
+        List<BigRange> result = []
+        for (Entry entry : entries) {
+            List<BigRange> newCurrentRanges = []
+            for (BigRange currentRange : currentRanges) {
+                if (currentRange.overlaps(entry.sourceRange)) {
+                    if (currentRange.from < entry.sourceRange.from) {
+                        newCurrentRanges << new BigRange(currentRange.from, entry.sourceRange.from - 1)
+                    }
+                    if (currentRange.to > entry.sourceRange.to) {
+                        newCurrentRanges << new BigRange(entry.sourceRange.to + 1, currentRange.to)
+                    }
+                    result << entry.sourceRange.intersect(currentRange).transpose(entry.diff)
+                } else {
+                    newCurrentRanges << currentRange
+                }
+            }
+            currentRanges = newCurrentRanges
+        }
+        result.addAll(currentRanges)
+        return result
+    }
+
+    protected static BigRange findSeedRange(List<List<Entry>> almanac, BigRange currentRange) {
+        def sorted = almanac.pop().sort()
+        BigRange result
+        long previous = 0
+
+        if (currentRange) {
+            for (int i = 0; i < sorted.size(); i++) {
+                def entry = sorted[i]
+                long max = (i == (sorted.size() - 1)) ? Long.MAX_VALUE : (sorted[i + 1].destination - 1)
+                result = new BigRange(previous, max)
+                if (result.overlaps(currentRange)) {
+                    break
+                } else {
+                    previous = max
+                }
+            }
+        }
+        if (almanac) {
+            result = findSeedRange(almanac, result)
         }
 
         return result
@@ -62,26 +110,91 @@ class Day5 {
         almanac
     }
 
-    static class Entry {
+    static class BigRange implements Comparable<BigRange> {
+        long from
+        long to
+
+        BigRange(String s) {
+            def tokenize = s.tokenize('-')
+            this.from = tokenize[0] as long
+            this.to = tokenize[1] as long
+        }
+
+        BigRange(long from, long to) {
+            this.from = from
+            this.to = to
+        }
+
+        boolean overlaps(BigRange other) {
+            if (this.from <= other.from) {
+                if (this.to >= other.from) {
+                    return true
+                }
+            } else {
+                if (this.to <= other.to || this.from <= other.to) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        BigRange intersect(BigRange other) {
+            return new BigRange([this.from, other.from].max(), [this.to, other.to].min())
+        }
+
+        BigRange transpose(long diff) {
+            return new BigRange(this.from + diff, this.to + diff)
+        }
+
+        @Override
+        String toString() {
+            return "$from-$to"
+        }
+
+        @Override
+        boolean equals(Object o) {
+            return o instanceof BigRange && this.from == o.from && this.to == o.to
+        }
+
+        @Override
+        int compareTo(BigRange o) {
+            return this.from <=> o.from
+        }
+    }
+
+    static class Entry implements Comparable<Entry> {
+        private long zero
+        private long one
+        private long two
         private long destination
         private long source
         private long range
         private long diff
         private long start
         private long end
+        private BigRange sourceRange
 
         Entry(String s) {
             def tokenize = s.tokenize().collect { it as long }
             destination = tokenize[0]
+            zero = destination
             source = tokenize[1]
-            range = tokenize[2]
+            one = source
+            this.range = tokenize[2]
+            two = this.range
             start = source
-            end = source + range
+            end = source + this.range
             diff = destination - source
+            sourceRange = new BigRange(source, source + range - 1)
         }
 
         String toString() {
-            return "$destination $source $range"
+            return "$destination $source ${this.range}"
+        }
+
+        @Override
+        int compareTo(Entry o) {
+            (destination + diff) <=> (o.destination + o.diff)
         }
     }
 }
--- src/test/groovy/aoc/y2023/day5/Day5Spec.groovy
+++ src/test/groovy/aoc/y2023/day5/Day5Spec.groovy
@@ -45,8 +45,8 @@ humidity-to-location map:
         Day5.part1(input) == output
 
         where:
-        input  | output
-        t1     | 35
+        input | output
+        t1    | 35
     }
 
     def "part1"() {
@@ -59,12 +59,45 @@ humidity-to-location map:
         Day5.part2(input) == output
 
         where:
-        input  | output
-        t1     | 46
+        input | output
+        t1    | 46
+    }
+
+    def "find ranges"() {
+        expect:
+        Day5.findRanges(entries.collect { new Day5.Entry(it) }, input.collect { new Day5.BigRange(it) }).sort() == output.collect { new Day5.BigRange(it) }.sort()
+
+        where:
+        entries                 | input            | output
+        ['50 98 2', '52 50 48'] | ['0-99']         | ['0-49', '52-98', '50-51']
+        ['50 98 2', '52 50 48'] | ['79-92']        | ['81-94']
+        ['10 15 5']             | ['0-19']         | ['0-14', '10-14']
+        ['0 69 1', '1 0 69']    | ['60-70']        | ['61-69', '0-0', '70-70']
+        ['100 150 10']          | ['0-0', '10-20'] | ['0-0', '10-20']
+        ['10 15 5']             | ['0-30']         | ['0-14', '10-15', '20-30']
+        ['10 15 5']             | ['0-12']         | ['0-12']
+        ['10 15 5']             | ['0-17']         | ['0-14', '10-12']
+        ['10 15 5']             | ['20-30']        | ['20-30']
+        ['10 15 5']             | ['17-30']        | ['12-15', '20-30']
+    }
+
+    def "overlap"() {
+        expect:
+        new Day5.BigRange(input).overlaps(new Day5.BigRange(input2)) == output
+        new Day5.BigRange(input2).overlaps(new Day5.BigRange(input)) == output
+
+        where:
+        input  | input2  | output
+        '5-10' | '5-10'  | true
+        '5-10' | '0-7'   | true
+        '5-10' | '7-8'   | true
+        '5-10' | '8-12'  | true
+        '5-10' | '0-4'   | false
+        '5-10' | '11-15' | false
     }
 
     def "part2"() {
         expect:
-        Day5.part2(InputReader.read("y2023/day5")) == 2254687 //too high
+        Day5.part2(InputReader.read("y2023/day5")) == 2254686
     }
 }
