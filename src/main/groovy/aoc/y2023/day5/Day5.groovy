a85497951833411ac5c5b3542f9ab733e7668ffc
James Cook <james.cook@mettle.co.uk>
2023-12-05 12:14:06 +0000

    2023/5 Part 2 brute force

--- src/main/groovy/aoc/y2023/day5/Day5.groovy
+++ src/main/groovy/aoc/y2023/day5/Day5.groovy
@@ -4,25 +4,13 @@ class Day5 {
     static part1(String inputString) {
         def input = inputString.tokenize('\n')
         List<Long> seeds = input.remove(0).tokenize(':')[1].tokenize().collect { it as long }
-        List<List<Entry>> almanac = []
-        List<Entry> current = []
-        for (String line : input) {
-            if (line) {
-                if (line.endsWith(':')) {
-                    current = []
-                    almanac << current
-                } else {
-                    current << new Entry(line)
-                }
-            }
-        }
+        List<List<Entry>> almanac = createAlmanac(input)
 
         List<Long> locations = seeds.collect { seed ->
             for (List<Entry> thing : almanac) {
                 for (Entry entry : thing) {
-                    def newSeed = entry.apply(seed)
-                    if (newSeed != seed) {
-                        seed = newSeed
+                    if (seed >= entry.start && seed <= entry.end) {
+                        seed += entry.diff
                         break
                     }
                 }
@@ -36,14 +24,28 @@ class Day5 {
     static part2(String inputString) {
         def input = inputString.tokenize('\n')
         List<Long> seedRanges = input.remove(0).tokenize(':')[1].tokenize().collect { it as long }
-        List<Long> seeds = []
+        List<List<Entry>> almanac = createAlmanac(input)
+
+        long result = Long.MAX_VALUE;
         for (int i = 0; i < seedRanges.size(); i++) {
-            for (j in (seedRanges[i])..<(seedRanges[i] + seedRanges[i+1])) {
-                seeds << j
+            for (seed in (seedRanges[i])..<(seedRanges[i] + seedRanges[i+1])) {
+                for (List<Entry> thing : almanac) {
+                    for (Entry entry : thing) {
+                        if (seed >= entry.start && seed <= entry.end) {
+                            seed += entry.diff
+                            break
+                        }
+                    }
+                }
+                if (seed < result) result = seed
             }
             i++
         }
 
+        return result
+    }
+
+    protected static List<List<Entry>> createAlmanac(List<String> input) {
         List<List<Entry>> almanac = []
         List<Entry> current = []
         for (String line : input) {
@@ -56,21 +58,7 @@ class Day5 {
                 }
             }
         }
-
-        List<Long> locations = seeds.collect { seed ->
-            for (List<Entry> thing : almanac) {
-                for (Entry entry : thing) {
-                    def newSeed = entry.apply(seed)
-                    if (newSeed != seed) {
-                        seed = newSeed
-                        break
-                    }
-                }
-            }
-            seed
-        }
-
-        return locations.min()
+        almanac
     }
 
     static class Entry {
@@ -78,20 +66,21 @@ class Day5 {
         private long source
         private long range
         private long diff
+        private long start
+        private long end
 
         Entry(String s) {
             def tokenize = s.tokenize().collect { it as long }
             destination = tokenize[0]
             source = tokenize[1]
             range = tokenize[2]
+            start = source
+            end = source + range
             diff = destination - source
         }
 
-        long apply(long seed) {
-            if (seed >= source && seed <= (source + range)) {
-                seed += diff
-            }
-            seed
+        String toString() {
+            return "$destination $source $range"
         }
     }
 }
