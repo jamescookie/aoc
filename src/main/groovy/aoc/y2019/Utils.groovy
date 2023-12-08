8fd90402d9250747a55aae5ace5e163a39332b1a
James Cook <james.cook@mettle.co.uk>
2023-12-08 10:09:16 +0000

    Docs

--- src/main/groovy/aoc/y2019/Utils.groovy
+++ src/main/groovy/aoc/y2019/Utils.groovy
@@ -1,7 +1,19 @@
 package aoc.y2019
 
 class Utils {
-    static long gcd(long m, long n) { m = m.abs(); n = n.abs(); n == 0 ? m : m % n == 0 ? n : gcd(n, m % n) }
+    /**
+     * Greatest Common Divisor
+     * Uses Euclidean algorithm:
+     * gcd(a, b) = gcd(|a%b|, |a|); where |a| >= |b|
+     * gcd(p, 0) = gcd(0, p) = |p|
+     */
+    static long gcd(long a, long b) { a = a.abs(); b = b.abs(); b == 0 ? a : a % b == 0 ? b : gcd(b, a % b) }
 
-    static long lcm(long m, long n) { Math.abs(m * n) / gcd(m, n) }
+    /**
+     * Lowest Common Multiple
+     * The absolute value of the product of two numbers is equal to the product of their GCD and LCM.
+     * As stated: gcd(a, b) * lcm(a, b) = |a * b|
+     * Consequently: lcm(a, b) = |a * b|/gcd(a, b)
+     */
+    static long lcm(long a, long b) { Math.abs(a * b) / gcd(a, b) }
 }
