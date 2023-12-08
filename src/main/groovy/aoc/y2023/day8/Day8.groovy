7e4d6e6e77dc4b0159ec56d393fd2d8a7bbce1dc
James Cook <james.cook@mettle.co.uk>
2023-12-08 09:04:10 +0000

    2023/8 Part 2 - brute force taking too long

--- src/main/groovy/aoc/y2023/day8/Day8.groovy
+++ src/main/groovy/aoc/y2023/day8/Day8.groovy
@@ -30,7 +30,31 @@ class Day8 {
     }
 
     static part2(String inputString) {
-        Integer[] input = inputString.tokenize().collect { it as int }
-        return null
+        def input = inputString.tokenize('\n')
+        def instructions = input.pop().toCharArray().collect {it == ('R' as char) ? 1 : 0}
+        Map<String, List<String>> maps = input.collectEntries {
+            def line = it.split(' = ')
+            [(line[0]): line[1][1..-2].split(', ')]
+        }
+        long stepCount = 0
+        boolean found = false
+        def possiblePaths = maps.keySet().findAll {it.endsWith('A') }.collect {maps[it]}
+        def instructionPointer = 0
+        def instruction = instructions[instructionPointer]
+
+        while (!found) {
+            stepCount++
+            def nextPaths = possiblePaths.collect {it[instruction]}
+            if (nextPaths.every {it.endsWith('Z')}) {
+                found = true
+            } else {
+                possiblePaths = maps.keySet().findAll {nextPaths.contains(it) }.collect {maps[it]}
+                instructionPointer++
+                if (instructionPointer == instructions.size()) instructionPointer = 0
+                instruction = instructions[instructionPointer]
+            }
+        }
+
+        return stepCount
     }
 }
