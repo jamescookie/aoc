26cf0b1a46136a050c3571c128345947301c8d07
James Cook <james.cook@mettle.co.uk>
2023-12-09 09:48:02 +0000

    2023/9 Part 1

---
+++ src/main/groovy/aoc/y2023/day9/Day9.groovy
@@ -0,0 +1,37 @@
+package aoc.y2023.day9
+
+class Day9 {
+    static part1(String inputString) {
+        def input = inputString.tokenize('\n')*.tokenize(' ')*.collect { it as int }
+        long result = 0
+
+        for (def row in input) {
+            def history = [row]
+            while (!history[-1].every {it == 0}) {
+                history.add(diffs(history[-1]))
+            }
+            history = history.reverse()
+            history.pop()
+            int addition = history.pop()[0]
+            for (i in 0..<history.size()) {
+                addition = history[i][-1] + addition
+            }
+            result += addition
+        }
+
+        return result
+    }
+
+    static part2(String inputString) {
+        Integer[] input = inputString.tokenize().collect { it as int }
+        return null
+    }
+
+    static List<Integer> diffs(List<Integer> input) {
+        List<Integer> result = []
+        for (i in 0..<(input.size() - 1)) {
+            result.add(input[i + 1] - input[i])
+        }
+        return result
+    }
+}
---
+++ src/test/groovy/aoc/y2023/day9/Day9Spec.groovy
@@ -0,0 +1,40 @@
+package aoc.y2023.day9
+
+import aoc.InputReader
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class Day9Spec extends Specification {
+    static def t1 = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+    def "part1 warmup tests"() {
+        expect:
+        Day9.part1(input) == output
+
+        where:
+        input  | output
+        t1     | 114
+    }
+
+    def "part1"() {
+        expect:
+        Day9.part1(InputReader.read("y2023/day9")) == 0
+    }
+
+    def "part2 warmup tests"() {
+        expect:
+        Day9.part2(input) == output
+
+        where:
+        input  | output
+        t1     | 2
+    }
+
+    def "part2"() {
+        expect:
+        Day9.part2(InputReader.read("y2023/day9")) == 0
+    }
+}
