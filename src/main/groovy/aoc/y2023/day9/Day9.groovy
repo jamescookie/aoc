dcf8cfa0954aa0b2c3397476cae111d045ca60db
James Cook <james.cook@mettle.co.uk>
2023-12-09 10:15:15 +0000

    2023/9 Part 2 - tidy

--- src/main/groovy/aoc/y2023/day9/Day9.groovy
+++ src/main/groovy/aoc/y2023/day9/Day9.groovy
@@ -24,24 +24,10 @@ class Day9 {
         while (!history[-1].every { it == 0 }) {
             history.add(diffs(history[-1]))
         }
-        history = history.reverse()
-        history.pop()
-        int addition = history.pop()[0]
-        for (i in 0..<history.size()) {
-            if (forwards) {
-                addition = history[i][-1] + addition
-            } else {
-                addition = history[i][0] - addition
-            }
-        }
-        return addition
+        return history.reverse().inject(0) {a, b -> forwards ? b[-1] + a : b[0] - a}
     }
 
     static List<Integer> diffs(List<Integer> input) {
-        List<Integer> result = []
-        for (i in 0..<(input.size() - 1)) {
-            result.add(input[i + 1] - input[i])
-        }
-        return result
+        return input[0..-2].withIndex().collect { it, i -> input[i + 1] - it }
     }
 }
